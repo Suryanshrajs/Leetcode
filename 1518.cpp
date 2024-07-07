870effe19880f31c1ee5ba39278ba03cee05d864
Suryanshrajs <suryansh.rajs96@gmail.com>
2024-07-07 10:50:56 +0530

    Create 1518.cpp

---
+++ 1518.cpp
@@ -0,0 +1,52 @@
+
+Time Complexity : O(n)
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+
+        if(numBottles<numExchange){
+            return numBottles;
+        }
+        
+        int drink = numBottles;
+        int free = numBottles;
+
+        while(free>=numExchange){
+            drink += (free/numExchange);
+            free = (free/numExchange)+(free%numExchange);
+        }
+
+        return drink;
+        
+    }
+};
+
+
+
+Using Recursion
+class Solution {
+public:
+
+    int solveUsingRecursion(int &numBottles, int &numExchange, int &drink, int &free){
+         if(numBottles<numExchange){
+            return numBottles;
+        }
+
+        if(free<numExchange){
+            return drink;
+        }
+
+        drink += (free/numExchange);
+        free = (free/numExchange)+(free%numExchange);
+
+    return solveUsingRecursion(numBottles, numExchange, drink, free);
+    }
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drink = numBottles;
+        int free = numBottles;
+        int ans = solveUsingRecursion(numBottles, numExchange,drink, free);
+        return ans;
+        
+    }
+};
